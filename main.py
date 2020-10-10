cc877dac749022eeeed484bd265d8a48c6c40e1f
guoliwen <guoliwen@hetao101.com>
2020-10-10 14:22:55 +0800

    add requirement.txt

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-main.py
+from flask import Flask
+
+app =Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return "hi flask"
+
+if __name__ == '__main__':
+    print(__name__)
+    app.run(host='0.0.0.0', port=1991, debug=True)
